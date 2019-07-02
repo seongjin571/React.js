f4b5c01e8c1e1b6796719b6b9b4b0790f5e23d0f
seongjin571 <gkstjdwls11@naver.com>
2019-07-03 01:24:21 +0900

    chap05 rf

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 // import MyComponent from './MyComponent'; 
 // import EventPractice from './EventPractice';
-import ValidationSample from './ValidationSample';
+// import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component{
-render(){
-  const a = "aaa";
+class App extends Component {
+  render() {
+    const a = "aaa";
     return (
       // <MyComponent name="한성진" age={27}/>
-      <ValidationSample />
+      <div>
+        <ScrollBox ref={(ref) => this.ScrollBox = ref} />
+        <button onClick={() => this.ScrollBox.ScrollToBottom()}>맨 밑으로</button>
+      </div>
     );
   }
 }
--- src/ValidationSample.js
+++ src/ValidationSample.js
@@ -16,12 +16,14 @@ class ValidationSample extends Component {
         this.setState({
             clicked: true,
             validated: this.state.password === '0000'
-        })
+        });
+        this.input.focus();
     }
     render() {
         return (
             <div>
-                <input type="password"
+                <input ref = {(ref) => {this.input = ref}}
+                type="password"
                 value = {this.state.password}
                 onChange = {this.handleChange}
                 onClick = {this.handleButtonClick}
